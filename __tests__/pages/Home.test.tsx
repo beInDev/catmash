c400a0cfa42f1a79a02e8c989ec78bae1279db2b
Wilfried EVIEUX <wilfried.ev@gmail.com>
2020-10-30 23:43:02 +0100

    refactor(general): format files

--- __tests__/pages/Home.test.tsx
+++ __tests__/pages/Home.test.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import Home from '../../pages/index'
-import renderer from 'react-test-renderer'
+import React from "react";
+import { render } from "@testing-library/react";
+import Home from "../../pages/index";
+import renderer from "react-test-renderer";
 
-
-test('renders deploy link', () => {
-  const { getByText } = render(<Home />)
+test("renders deploy link", () => {
+  const { getByText } = render(<Home />);
   const linkElement = getByText(
     /Instantly deploy your Next\.js site to a public URL with Vercel\./
-  )
-  expect(linkElement).toBeInTheDocument()
-})
+  );
+  expect(linkElement).toBeInTheDocument();
+});
 
-it('renders homepage unchanged', () => {
-  const tree = renderer.create(<Home />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+it("renders homepage unchanged", () => {
+  const tree = renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- setupJest.js
+++ setupJest.js
@@ -1,2 +1,2 @@
 // Imports jest dom library for easier testing
-import '@testing-library/jest-dom/extend-expect'
+import "@testing-library/jest-dom/extend-expect";
