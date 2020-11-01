508fa2e27596807de8fa601fda8bd58809ad350a
Wilfried EVIEUX <wilfried.ev@gmail.com>
2020-11-01 18:36:25 +0100

    refactor(api): remove unused [id] endpoint

--- pages/api/score/details/[id].ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { createHandler } from "utils/handlers.utils";
-
-export default createHandler(
-  ["GET"],
-  (req: NextApiRequest, res: NextApiResponse) => {
-    const { id } = req.
-  }
-);
